4a8f97ad3b844753a9170e565626a48462bb0eb5
pooja2022ca059 <pooja.2022ca059@mnnit.ac.in>
2025-09-03 03:59:50 +0530

    origin changed

--- src/main/java/com/aipm/ai_project_management/config/WebConfig.java
+++ src/main/java/com/aipm/ai_project_management/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173") // Yahan apna frontend origin lagao
+                        .allowedOrigins("https://frontend-indol-sigma-48.vercel.app") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
